97dd8fd1be0fb925bf193146b5cff58dfcf0f64a
Rory Stokes <helixwhiza@gmail.com>
2012-09-09 14:40:44 +0930

    more code

--- main.py
+++ main.py
@@ -5,3 +5,30 @@ import world
 import entity
 import render
 import event
+
+
+window = pygame.display.set_mode((1024,600),pygame.RESIZABLE)
+fpsClock = pygame.time.Clock()
+event_manager = event.Event()
+# world = world.World(event_manager,window)
+
+running = True
+def close():
+    global running
+    running = False
+event_manager.register("quit", close)
+
+while running:
+    fpsClock.tick(30)
+    event_manager.notify("update", fpsClock.get_time()/1000.0)
+    event_manager.update()
+
+    eventlist = pygame.event.get()
+    for e in eventlist:
+        if e.type == QUIT:
+            print "quit"
+            event_manager.notify("quit")
+        elif e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                event_manager.notify("quit")
+                
