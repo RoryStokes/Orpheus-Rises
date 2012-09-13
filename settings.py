1345b2d17b45a3c2e62f5eaf5152185fd44c73b2
keeeal <keal72@gmail.com>
2012-09-13 18:37:09 +0930

    Added flowing water (right only).

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ entities = []
 entities.append(Entity('img/character.png'))
 
 
-# Tiles
+ # Tiles
 tiles = []
  # 0: spawn
 tiles.append( Tile( 1, 0,-1) )
@@ -26,14 +26,27 @@ tiles.append( Tile( 1,-1,-1) )
 tiles.append( Tile( 2,-1,-1) )
  # 3: void
 tiles.append( Tile( 0,-1,-1) )
- # 4: snakes a
-tiles.append( Tile(-1,-1, 8) )
- # 5: lava a
+ # 4: lava a
+tiles.append( Tile(-1,-1, 5) )
+ # 5: lava b
 tiles.append( Tile( 0,-1, 6) )
- # 6: lava b
+ # 6: lava c
 tiles.append( Tile( 0,-1, 7) )
- # 7: lava c
-tiles.append( Tile( 0,-1, 5) )
- # 8: snakes b
-tiles.append( Tile(-1,-1, 4) )
- 
+ # 7: lava d
+tiles.append( Tile( 0,-1, 4) )
+ # 8: snakes a
+tiles.append( Tile( 1,-1, 9) )
+ # 9: snakes b
+tiles.append( Tile( 1,-1, 8) )
+ # 10: blue
+tiles.append( Tile( 1,-1,-1) )
+ # 11: pink
+tiles.append( Tile( 1,-1,-1) )
+ # 12: water_right a
+tiles.append( Tile( 1,-1, 13) )
+ # 13: water_right b
+tiles.append( Tile( 1,-1, 14) )
+ # 14: water_right c
+tiles.append( Tile( 1,-1, 15) )
+ # 15: water_right d
+tiles.append( Tile( 1,-1, 12) )
